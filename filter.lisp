7b89aeb67531ea085683ddd168dd921905f57253
Tony <cardiox1212@hotmail.fr>
2022-05-26 18:39:39 +0200

    add: filter.lisp and packages

---
+++ filter.lisp
@@ -0,0 +1,53 @@
+(defpackage :com.bbellavi.spam
+  (:use :common-lisp :com.bbellavi.pathnames))
+
+(defparameter *max-ham-score* .4)
+(defparameter *min-spam-score* .6)
+(defvar *feature-database* (make-hash-table :test #'equal))
+
+(defclass word-feature ()
+  ((word
+    :initarg :word
+    :accessor word
+    :initform (error "Must supply :word")
+    :documentation "The word this feature represents.")
+   (spam-count
+    :initarg :spam-count
+    :accessor spam-count
+    :initform 0
+    :documentation "Number of spams we have seen this feature in.")
+   (ham-count
+    :initarg :ham-count
+    :accessor ham-count
+    :initform 0
+    :documentation "Number of hams we have seen this feature in.")))
+
+(defmethod print-object ((object word-feature) stream)
+  (print-unreadable-object (object stream :type t)
+    (with-slots (word ham-count spam-count) object
+      (format stream "~s :hams ~d :spams ~d" word ham-count spam-count))))
+
+(defun clear-database ()
+  (setf *feature-database* (make-hash-table :test #'equal)))
+
+(defun intern-feature (word)
+  (or (gethash word *feature-database*)
+      (setf (gethash word *feature-database*)
+            (make-instance 'word-feature :word word))))
+
+(defun extract-words (text)
+  (delete-duplicates
+   (cl-ppcre:all-matches-as-strings "[a-zA-Z]{3,}" text)
+   :test #'string=))
+
+(defun extract-features (text)
+  (mapcar #'intern-feature (extract-words text)))
+
+(defun classification (score)
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t                          'unsure)))
+
+(defun classify (text)
+  (classification (score (extract-features text))))
